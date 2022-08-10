3f1bfec761036e9b6ee039d73e29e138bfde289a
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-08-10 16:45:05 -0300

    Add ViewModel to add a Category

---
+++ ViewModels/CreateCategoryViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.ViewModels
+{
+    public class CreateCategoryViewModel
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}

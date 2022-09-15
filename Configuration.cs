ed71016d4b74874109691421bd4c61002b668b6d
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-09-14 22:22:52 -0300

    Add JWT configuration with key

---
+++ Configuration.cs
@@ -0,0 +1,8 @@
+namespace BlogAPI_estudos;
+
+
+public static class Configuration
+{
+    //TOKEN - JWT
+    public static string JwtKey { get; set; } = "FU4Uja8HbDJp67qk8DSNNMLfjM8SqEGpOe4pHCNOaQ35HBqft8";
+}

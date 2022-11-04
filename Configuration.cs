ddaba88a402acd9cccd72895edd26ebfc612c9f7
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-11-03 22:55:08 -0300

    Setting auth configurations

--- Configuration.cs
+++ Configuration.cs
@@ -4,5 +4,7 @@ namespace Blog;
 public static class Configuration
 {
     //TOKEN - JWT
-    public static string JwtKey { get; set; } = "FU4Uja8HbDJp67qk8DSNNMLfjM8SqEGpOe4pHCNOaQ35HBqft8";
+    public static string JwtKey = "FU4Uja8HbDJp67qk8DSNNMLfjM8SqEGpOe4pHCNOaQ35HBqft8";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_md8@E!F14Va75fmaj1%¨GAFA51a62AH-X=Z";
 }

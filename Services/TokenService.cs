e1333ac56dbb403080167dea0ffd8034c7860bd3
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-11-03 22:57:16 -0300

    Add method for JWT generation

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Blog.Extensions;
 using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 
@@ -11,15 +12,10 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClaims();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new (ClaimTypes.Name, "fernando.macedo"),
-                new (ClaimTypes.Role, "user"),
-                new (ClaimTypes.Role, "admin"),
-                new ("fruta","banana"),
-    }),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key),

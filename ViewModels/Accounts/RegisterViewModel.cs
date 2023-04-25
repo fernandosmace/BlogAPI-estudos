3ca6779aa772b7f58496c8c9e4e5b2d5edde076e
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-04-24 21:38:09 -0300

    Add ViewModel to login

---
+++ ViewModels/Accounts/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels.Accounts;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "O campo Nome é obrigatório!")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "O campo Email é obrigatório!")]
+    [EmailAddress(ErrorMessage = "O e-mail informado é inválido")]
+    public string Email { get; set; }
+
+    [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "O senha deve ter no mínimo 8 caracteres com letras maiúsculas e minúsculas, números e caracteres especiais.")]
+    public string Password { get; set; }
+}

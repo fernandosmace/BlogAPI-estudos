c7fd196fc0516b036db86d3f619bcc2a30cc652c
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-04-24 21:42:20 -0300

    Add others improvements to the application

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -6,12 +6,11 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public BlogDataContext(DbContextOptions<BlogDataContext> options) : base(options){}
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(System.Environment.GetEnvironmentVariable("DEFAULT_CONNECTION"));
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

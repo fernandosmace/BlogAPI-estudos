2bc029a0e734139ce7f565d2ad65b9beedc3cf2f
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-04-24 21:41:43 -0300

    Add Smtp configuration to email service

--- Configuration.cs
+++ Configuration.cs
@@ -7,4 +7,13 @@ public static class Configuration
     public static string JwtKey = "FU4Uja8HbDJp67qk8DSNNMLfjM8SqEGpOe4pHCNOaQ35HBqft8";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "curso_api_md8@E!F14Va75fmaj1%¨GAFA51a62AH-X=Z";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 }
